6356b0e571d2cc88b23e3136d687e6af401d0149
Alejandro Mejía <alejandro.mejia@bairesdev.com>
2023-10-06 09:27:12 -0500

    Add more routes

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,4 +1,11 @@
 export const ROUTES = {
   login: '/',
-  home: '/home'
+  home: '/home',
+  explore: '/explore',
+  notifications: '/notifications',
+  messages: '/messages',
+  lists: '/{user}/lists',
+  communities: '/{user}/communities',
+  verified: '/verified-choose',
+  profile: '/{user}'
 }
